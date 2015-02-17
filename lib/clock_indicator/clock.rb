990be0aab06d078cc1b6160a459b39aacaf04680
Juan C. Müller <jcmuller@gmail.com>
2015-02-16 20:34:32 -0500

    Show full time in context menu. Position menu better.

--- lib/clock_indicator/clock.rb
+++ lib/clock_indicator/clock.rb
@@ -27,7 +27,11 @@ module ClockIndicator
       end
 
       def tooltip_status
-        Time.now.to_s
+        Time.new.to_s
+      end
+
+      def time_item_text
+        Time.new.strftime("%A, %B %e, %Y %I:%M:%S %P")
       end
 
       def sub_menu
@@ -40,9 +44,13 @@ module ClockIndicator
           when 1
             toggle_calendar
           when 3
-            sub_menu.popup(nil, nil, nil, nil, event.button, event.time)
+            popup_sub_menu(event)
           end
         end
+      end
+
+      def pos(event)
+        proc { [event.x_root, 19] }
       end
 
       def toggle_calendar
@@ -52,6 +60,11 @@ module ClockIndicator
           calendar_window.destroy
           @calendar_window = nil
         end
+      end
+
+      def popup_sub_menu(event)
+        gtk_update_ui
+        sub_menu.popup(nil, nil, pos(event), nil, event.button, event.time)
       end
 
       def calendar_window
@@ -85,29 +98,15 @@ module ClockIndicator
       end
 
       def update_ui
-        run_in_gtk = Proc.new do
-          gtk_update_ui
-        end
+        run_in_gtk = proc { gtk_update_ui }
         GLib.idle_add(GLib::PRIORITY_DEFAULT_IDLE, run_in_gtk, nil, nil)
-      rescue Exception => e
-        show_notification("Clock.rb error", "#{e.class}: #{e.message}")
       end
 
       def gtk_update_ui
         tray_label.set_markup(time)
         tray_icon.set_tooltip_text(tooltip_status)
+        sub_menu.set_time(time_item_text)
       end
 
-      def show_notification(title, message)
-        dialog = Gtk::Dialog.new
-        dialog.set_title(title)
-        label = Gtk::Label.new(message)
-        dialog.get_content_area.add(label)
-        dialog.add_button('_OK', -1)  # GTK_RESPONSE_NONE == -1
-        GObject.signal_connect(dialog, 'response') do |d, response_id, user_data|
-          d.destroy
-        end
-        dialog.show_all
-      end
   end
 end
--- lib/clock_indicator/sub_menu.rb
+++ lib/clock_indicator/sub_menu.rb
@@ -3,19 +3,37 @@ module ClockIndicator
 
     delegate :popup, :to => :menu
 
+    def set_time(time)
+      time_item.set_label(time)
+    end
+
     private
 
       def menu
         @menu ||= Gtk::Menu.new.tap do |m|
-          m.append(item)
+          m.append(time_item)
+          m.append(separator)
+          m.append(exit_item)
           m.show_all
         end
       end
 
-      def item
-        @item ||= Gtk::MenuItem.new_with_label("Exit").tap do |i|
+      def time_item
+        @time_item ||= Gtk::MenuItem.new.tap do |i|
+          # Not selectable
+          i.signal_connect("select") { i.deselect }
+        end
+      end
+
+      def separator
+        @separator ||= Gtk::SeparatorMenuItem.new
+      end
+
+      def exit_item
+        @exit_item ||= Gtk::MenuItem.new_with_label("Quit").tap do |i|
           i.signal_connect("activate") { Gtk.main_quit }
         end
       end
+
   end
 end
